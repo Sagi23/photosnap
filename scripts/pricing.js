ae9d7e941f7e793ff2f2c1256bd4e4dfc2a296de
Sagi Twig <twigsagi@gmail.com>
2020-12-05 15:52:50 +0200

    Fixed toggle bug

--- scripts/pricing.js
+++ scripts/pricing.js
@@ -7,13 +7,13 @@ const year = document.querySelector(".year");
 toggle.addEventListener("click", function () {
   if (toggle.src.match("assets/pricing/desktop/toggle-month.svg")) {
     toggle.src = "assets/pricing/desktop/toggle-year.svg";
-    yearPrice.forEach((e) => e.classList.toggle("hide"));
+    yearPrice.forEach((e) => e.classList.toggle("change"));
     monthYear.forEach((e) => (e.textContent = "year"));
     year.classList.toggle("checked");
     month.classList.toggle("checked");
   } else if (toggle.src.match("assets/pricing/desktop/toggle-year.svg")) {
     toggle.src = "assets/pricing/desktop/toggle-month.svg";
-    yearPrice.forEach((e) => e.classList.toggle("hide"));
+    yearPrice.forEach((e) => e.classList.toggle("change"));
     monthYear.forEach((e) => (e.textContent = "month"));
     year.classList.toggle("checked");
     month.classList.toggle("checked");
