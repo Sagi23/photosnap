f1e370c62efb824832dd236fa8484c4e7779df2e
Sagi Twig <twigsagi@gmail.com>
2020-12-05 15:48:33 +0200

    Fixed navbar bug

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,16 @@ const ul = document.querySelector("#ul");
 
 menuBtn.addEventListener("click", function () {
   if (menuBtn.src.match("assets/shared/mobile/menu.svg")) {
-    ul.style.display = "grid";
+    menuBtn.src = "assets/shared/mobile/close.svg";
+    ul.classList.toggle("hide");
     window.setTimeout(function () {
       ul.style.transform = "scale(1)";
     }, 0);
-    menuBtn.src = "assets/shared/mobile/close.svg";
   } else {
-    ul.style.transform = "scale(0)";
     menuBtn.src = "assets/shared/mobile/menu.svg";
+    ul.style.transform = "scale(0)";
     window.setTimeout(function () {
-      ul.style.display = "none";
+      ul.classList.toggle("hide");
     }, 400);
   }
 });
